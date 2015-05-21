03379747f1bc0059470b441acf5408c665d1d5ab
Joseph Chen <joseph.chen@gmail.com>
2015-05-21 07:40:39 -0700

    Add default puma values in case not set.  Also expose preload_app setting.

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,14 +12,15 @@ node[:deploy].each do |application, deploy|
   puma_config application do
     directory deploy[:deploy_to]
     environment deploy[:rails_env]
-    logrotate deploy[:puma][:logrotate]
-    thread_min deploy[:puma][:thread_min]
-    thread_max deploy[:puma][:thread_max]
-    workers deploy[:puma][:workers]
-    worker_timeout deploy[:puma][:worker_timeout]
-    restart_timeout deploy[:puma][:restart_timeout] || 120
-    exec_prefix deploy[:puma][:exec_prefix] || "bundle exec"
-    prune_bundler deploy[:puma][:prune_bundler]
+    logrotate deploy[:puma] && deploy[:puma][:logrotate] ? deploy[:puma][:logrotate] : true
+    thread_min deploy[:puma] && deploy[:puma][:thread_min] ? deploy[:puma][:thread_min] : 1
+    thread_max deploy[:puma] && deploy[:puma][:thread_max] ? deploy[:puma][:thread_max] : 16
+    workers deploy[:puma] && deploy[:puma][:workers] ? deploy[:puma][:workers] : 2
+    worker_timeout deploy[:puma] && deploy[:puma][:worker_timeout] ? deploy[:puma][:worker_timeout] : 30
+    restart_timeout deploy[:puma] && deploy[:puma][:restart_timeout] ? deploy[:puma][:restart_timeout] : 120
+    exec_prefixdeploy[:puma] && deploy[:puma][:exec_prefix] ? deploy[:puma][:exec_prefix] : 'bundle exec'
+    prune_bundler deploy[:puma] && deploy[:puma][:prune_bundler] ? deploy[:puma][:prune_bundler] : true
+    preload_app deploy[:puma] && deploy[:puma][:preload_app] ? deploy[:puma][:preload_app] : false
   end
 end
 
